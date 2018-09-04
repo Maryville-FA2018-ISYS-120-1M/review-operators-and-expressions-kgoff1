163bf00956c29917b91ad65d4deca630086b0794
Kenneth Goff <kgoff1@live.maryville.edu>
2018-09-03 17:15:26 -0700

    HW completion

--- ExpressionsAndAssignmentReview.playground/Contents.swift
+++ ExpressionsAndAssignmentReview.playground/Contents.swift
@@ -14,4 +14,27 @@
  You should write your story bits in comments (e.g., between /* and */, or lines starting with // ) around where the code should go.
  
 */
-
+// Three baseball players want to check their overall stats based on individual performance. We will first calculate their batting average.
+var hits1=Double(30)
+var hits2=Double(40)
+var hits3=Double(45)
+var atBats=Double(130)
+let player1AVG=hits1/atBats
+let player2AVG=hits2/atBats
+let player3AVG=hits3/atBats
+print(player1AVG,"Batting average of Player 1",player2AVG,"Batting average of Player 2",player3AVG,"Batting average of Player 3")
+// However, we need decimal points here, so let's go back and change the constraints to double values
+// Hits were given a variable setting, and today, player 2 and 3 both got an extra 3 hits, let's update the values
+hits2+=3
+hits3+=3
+atBats+=3
+// Today, the offical scorer ruled one of player 1's hits as an error, so we need to take 1 hit away from his total.
+hits1-=1
+// Now we can look at the updated official batting average of each player.
+let p1FinalAVG=hits1/atBats
+let p2FinalAVG=hits2/atBats
+let p3FinalAVG=hits3/atBats
+print(p1FinalAVG,"Batting average of Player 1",p2FinalAVG,"Batting average of Player 2",p3FinalAVG,"Batting average of Player 3")
+// As an example of a conversion, we will add player 1's hits from last year to this year
+var hitsY1=40
+let totalHits=Double(hitsY1)+hits1
